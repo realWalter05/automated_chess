d81d41ae2f9d208c875e5fb92b961caae2cab45e
realWalter05 <zikavaclav05@gmail.com>
2024-03-31 12:51:03 +0200

    update

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,35 @@
+
+enum Type {
+    NONE = 0,
+    PAWN,
+    ROOK,
+    BISHOP,
+    KNIGHT,
+    QUEEN,
+    KING,
+};
+
+enum Color {
+    BLACK = 1,
+    WHITE,
+};
+
+struct Piece {
+    enum Type type;
+    enum Color color;
+};
+
+int board[8][8] = {
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+};
+
 void setup() {
   // put your setup code here, to run once:
 
