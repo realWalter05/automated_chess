916b38ba169a60d36ee7d68e23a0281610aa2c02
Václav Zíka <zikavaclav05@gmail.com>
2024-11-10 20:55:36 +0100

    Control by console

--- main/main.ino
+++ main/main.ino
@@ -1,41 +1,91 @@
+#include <AccelStepper.h>
 
-enum Type {
-    NONE = 0,
-    PAWN,
-    ROOK,
-    BISHOP,
-    KNIGHT,
-    QUEEN,
-    KING,
-};
+// Define pin connections & motor's steps per revolution
+const int directionY = 11;
+const int directionX = 9;
+const int motorYPin = 10;
+const int motorXPin = 8;
 
-enum Color {
-    BLACK = 1,
-    WHITE,
-};
+const int transistorPin = 13;
+int transistorValue = HIGH;
 
-struct Piece {
-    enum Type type;
-    enum Color color;
-};
+// Define motor interface type
+#define motorInterfaceType 1
 
-int board[8][8] = {
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-};
+// Creates an instance
+AccelStepper motorY(motorInterfaceType, motorYPin, directionY);
+AccelStepper motorX(motorInterfaceType, motorXPin, directionX);
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(transistorPin, OUTPUT);
+  
+  motorX.setMaxSpeed(400);
+  motorY.setMaxSpeed(400);
 
+  Serial.begin(9600);
+  Serial.println("Where to move magnet (1235) or control transistor 9(on)-0(off): ");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // Control read
+    if (motorX.distanceToGo() == 0 && motorY.distanceToGo() == 0) {
 
+      int menuChoice = Serial.parseInt();
+      Serial.println(menuChoice);
+
+      switch (menuChoice) {
+        case 1:
+          motorX.move(100);
+          motorX.setSpeed(400);
+
+          break;
+
+        case 2:
+          motorY.move(-100);
+          motorY.setSpeed(150);
+
+          break;
+
+        case 3:
+          motorX.move(-100);
+          motorX.setSpeed(400);
+          break;
+
+        case 4:
+          motorY.move(-100);
+          motorX.move(100);
+          motorY.setSpeed(150);
+          motorX.setSpeed(400);
+
+          break;
+
+        case 5:
+          motorY.move(150);
+          motorY.setSpeed(200);
+          break;
+
+        case 6:
+          motorY.move(-100);
+          motorX.move(-100);
+          motorY.setSpeed(150);
+          motorX.setSpeed(400);
+
+          break;
+
+        case 9:
+          transistorValue = HIGH;
+          break;
+
+
+        case 8:
+          transistorValue = LOW;
+          break;
+
+        default:
+          Serial.println("Please choose a valid selection...");
+      }
+    }
+    digitalWrite(transistorPin, transistorValue);  
+    motorX.runSpeedToPosition();
+    motorY.runSpeedToPosition();
 }
